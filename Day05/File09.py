ba246fe19287796bb8615ed4e8b328b2e25d3148
lavnyasharma <lavanya2210sharma@gmail.com>
2023-02-24 14:22:25 +0530

    Updated

--- Day05/File09.py
+++ Day05/File09.py
@@ -1,1 +1,17 @@
 #The first line of input contains the integer K(1<-K<-45) , the number of times Mirko pressed the button.
+
+def count_letters(K):
+    word = "A"  
+    for i in range(K):
+        new_word = ""
+        for letter in word:
+            if letter == "A":
+                new_word += "B"
+            else:
+                new_word += "BA"
+        word = new_word
+
+    num_a = word.count("A")
+    num_b = word.count("B")
+    return num_a, num_b
+print(count_letters(10))
