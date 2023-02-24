235a283f861674781560e7fb1812db3e6ff56639
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-02-24 10:22:25 +0530

    Day 5 directory created & files added.

---
+++ Day05/File01.py
@@ -0,0 +1,11 @@
+# Linear search python.
+lst = [5, 4, 3, 2, 11, 7, 9, 12]
+class LinearSearch:
+    def search(self, lst, keyValue):
+        for i in range(len(lst)):
+            if lst[i] == keyValue:
+                return i
+        return 'Not Found'
+
+obj = LinearSearch()
+print(obj.search(lst, 1))
---
+++ Day05/File02.py
@@ -0,0 +1,18 @@
+# Binary search
+class BinarySearch:
+    def searchMethod(self, lst, low, high, keyValue):
+        self.lst = sorted(lst)
+        if high >= low:
+            mid = low+(high-1)//2
+            if lst[mid] == keyValue:
+                return mid
+            elif lst[mid] > keyValue:
+                return self.searchMethod(lst, low, mid-1, keyValue)
+            else:
+                return self.searchMethod(lst, mid+1, high, keyValue)
+        else:
+            return 'Not found'
+lis = [14,2,3,7,9,10,12,15]
+n = 12
+obj = BinarySearch()
+print(obj.searchMethod(lis, 0, len(lis)-1, n))
