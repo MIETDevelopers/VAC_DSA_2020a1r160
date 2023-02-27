4a011fe597e08e0c739524a24bab6e211d355e54
samragyi <samragyi1212@gmail.com>
2023-02-27 15:22:16 +0530

    Files added.

--- Day07/Queue/File01.py
+++ Day07/Queue/File01.py
@@ -1,2 +1,5 @@
 # linear data structure which stores objects in FIFO format. the least recently added element is removed first.
 # In a queue, we have two things --- Front and Rare
+# Adding an element is called Enqueue
+# Deleting an element is called Dequeue
+
