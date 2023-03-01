d6a28df62722d71aa5e3567bae3c97248e741d97
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-03-01 10:37:41 +0530

    Files added

---
+++ Day09/Tree/File03.py
@@ -0,0 +1,37 @@
+#Post order traversal without using recursion.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def postorderTraversal(root):
+    if not root:
+        return []
+
+    stack1 = [root]
+    stack2 = []
+
+    while stack1:
+        node = stack1.pop()
+        stack2.append(node)
+
+        if node.left:
+            stack1.append(node.left)
+        if node.right:
+            stack1.append(node.right)
+
+    result = []
+    while stack2:
+        node = stack2.pop()
+        result.append(node.val)
+
+    return result
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print(postorderTraversal(root))
