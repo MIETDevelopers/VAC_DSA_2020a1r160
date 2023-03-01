58592fcd15af89b60ba270c449728beb760f3fe7
samragyi <samragyi1212@gmail.com>
2023-03-01 14:01:23 +0530

    Files added.

---
+++ Day09/Problems/File05.py
@@ -0,0 +1,14 @@
+# format string --- add spaces
+def format_string(input_string):
+    formatted_string = ""
+    i = 0
+    while i < len(input_string):
+        if input_string[i].isupper() and i > 0:
+            formatted_string += " "
+        formatted_string += input_string[i].lower()
+        i += 1
+    return formatted_string
+
+input_string = "ThePairOfShoeCostedSomeBucks"
+formatted_string = format_string(input_string)
+print(formatted_string)
