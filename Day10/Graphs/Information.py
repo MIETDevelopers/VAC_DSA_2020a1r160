ea886a9d74d1fa7aef563d988b8e5906b0394815
Bhoomi <2020a1r009@mietjammu.in>
2023-03-02 14:02:37 +0530

    File Updated

--- Day10/Graphs/Information.py
+++ Day10/Graphs/Information.py
@@ -8,3 +8,8 @@
 #In case of directed graphs if there is edge between vI to vJ, we assign Graph of IJ as 1.
 #In case of undirected graphts we have to show there is a edge from i to j & Vice Versa.
 print('Graphssszzzz')
+#BFS and DFS
+#BFS: Breadth First Search
+#BFS: It is a traversing algorithm where you should start traversing from a selected node (source or starting node) and traverse the graph layerwise thus exploring the neighbour nodes (nodes which are directly connected to source node). You must then move towards the next-level neighbour nodes.
+#DFS: Depth First Search
+#DFS: It is a traversing algorithm where you should start traversing from a selected node (source or starting node) and traverse the graph as far as possible along each branch before backtracking.
