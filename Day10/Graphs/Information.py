4944c9eeb70686cac71d451f8bac5b8015b44ef8
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-03-02 10:45:54 +0530

    Files added

---
+++ Day10/Graphs/Information.py
@@ -0,0 +1,10 @@
+#Graph is a non linear data structure where it contains nodes or vertices connected theough edges or arcs
+#Where set of nodes & set of edges combine to used as represent graphs.
+#Types: Finite, Infinite: Ref: https://www.geeksforgeeks.org/graph-types-and-applications/
+#Complete Graph n Vetices & n-1 Nodes, one vertex attached to another. --- Full Graph
+#Regular graph: All number of vertices == Degree
+#Representation: Adjacant matrix & Adjacent List.
+#Adjacant matrix is a 2D Array which size v*v.
+#In case of directed graphs if there is edge between vI to vJ, we assign Graph of IJ as 1.
+#In case of undirected graphts we have to show there is a edge from i to j & Vice Versa.
+print('Graphssszzzz')
