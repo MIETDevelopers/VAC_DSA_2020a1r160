9c3add8c75040845da62ee357a3b16ee49e7a8f8
samragyi <samragyi1212@gmail.com>
2023-08-16 13:20:52 +0530

    day1

---
+++ Session_August/Day01/File01.py
@@ -0,0 +1,32 @@
+print(chr(3172))
+"""_summary_
+    range for data types in C
+
+signed int =  - 2147483648 to 214.....7
+unsigned - 0 to 4294967295
+
+ASCII Values
+ a - z = 97 to 122
+A- Z  = 65 to 90
+0-9 = 48 to 57
+space bar = 32
+
+
+chr() --- unicode to human understandable 
+ord() --- human understandable to unicode
+
+
+LCS - Longest Common Subsequences
+
+* the characters is a sub sequence may or may not be consecutive as in the parent sequence
+* subsequence are in the same direction - whenever there is an intersection of characters, do not consider the character which 
+is in the opposite direction
+
+implement by using
+- recursion
+memorization
+tabulation(dynamic programming)
+
+    """
+    
+    
