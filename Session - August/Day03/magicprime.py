f29eca49e0bfa37e2013e23c60a404e38e5a3c93
samragyi <samragyi1212@gmail.com>
2023-08-18 13:44:43 +0530

    Files added

---
+++ Session - August/Day03/magicprime.py
@@ -0,0 +1,5 @@
+"""
+    find all the prime numbers existing before that number and check none of its digit comprise of any even number (i.e. 0, 2, 4, 6, 8). 
+    If found, hignore that number and add the other appropriate numbers.
+    """
+    
