3ea24ca933534e1fb5b99e384651eaab76282a0e
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-08-19 13:42:26 +0530

    files added

--- Session - August/Day04/struct01.c
+++ Session - August/Day04/struct01.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 struct Apple{
-    int a[100];
-    float b[500];
-    char c[777];
+    int a[500];
+    double b[800];
+    char c[1000];
 }a1, a2;
 int main(){
     printf("Size of a1: %d\n", sizeof(a1));
