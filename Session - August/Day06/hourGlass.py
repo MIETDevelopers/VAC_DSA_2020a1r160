4fe0c23e4de692f5fd1816354505900cdd687a93
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-08-22 11:01:51 +0530

    files added

---
+++ Session - August/Day06/hourGlass.py
@@ -0,0 +1,11 @@
+def print_reverse_pyramid(height):
+    for i in range(height, 0, -1):
+        print(" " * (height - i) + "* " * i)
+
+def print_pyramid(height):
+    for i in range(2, height + 1):
+        print(" " * (height - i) + "* " * i)
+
+height = int(input("Enter the height of the reverse pyramid: "))
+print_reverse_pyramid(height)
+print_pyramid(height)
