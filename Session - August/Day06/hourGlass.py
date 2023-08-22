202aa10246153bee7ada2133c6ff2a8ae2c2797f
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-08-22 11:08:55 +0530

    files added

--- Session - August/Day06/hourGlass.py
+++ Session - August/Day06/hourGlass.py
@@ -6,6 +6,6 @@ def print_pyramid(height):
     for i in range(2, height + 1):
         print(" " * (height - i) + "* " * i)
 
-height = int(input("Enter the height of the reverse pyramid: "))
+height = int(input("Enter the height of the hourglass: "))
 print_reverse_pyramid(height)
 print_pyramid(height)
