07fe8804018b5d871e24a2c059a206ff5ac47d5c
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-08-22 10:09:37 +0530

    files added

--- Session - August/Day06/File01.py
+++ Session - August/Day06/File01.py
@@ -1,18 +1,57 @@
-#Create a program to print left side of a binary tree based on dynamic user input
-def print_left_side_of_tree(tree):
-    if tree == None:
-        return
-    print(tree.data)
-    print_left_side_of_tree(tree.left)
-    print_left_side_of_tree(tree.right)
-def input_dynamic_tree():
-    data = input("Enter data: ")
-    if data == "x":
+#A Program to print left view of a complete binary tree based on dynamic user input but first ask for height of tree
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def buildTree(height):
+    if height <= 0:
         return None
-    tree = Tree(data)
-    tree.left = input_dynamic_tree()
-    tree.right = input_dynamic_tree()
-    return tree
+    
+    root = TreeNode(int(input("Enter value for root: ")))
+    queue = [root]
+    level = 1
+    while level < height:
+        node = queue.pop(0)
+        
+        left_val = int(input(f"Enter left child value for {node.val}: "))
+        if left_val != -1:
+            node.left = TreeNode(left_val)
+            queue.append(node.left)
+        
+        right_val = int(input(f"Enter right child value for {node.val}: "))
+        if right_val != -1:
+            node.right = TreeNode(right_val)
+            queue.append(node.right)
+        
+        level += 1
+    
+    return root
+
+def leftView(root):
+    if not root:
+        return
+    
+    queue = [root]
+    while queue:
+        count = len(queue)
+        for i in range(count):
+            node = queue.pop(0)
+            if i == 0:
+                print(node.val, end=" ")
+            
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
 def main():
-    tree = input_dynamic_tree()
-    print_left_side_of_tree(tree)
+    height = int(input("Enter the height of the binary tree: "))
+    root = buildTree(height)
+    
+    print("Left view of the binary tree:")
+    leftView(root)
+
+if __name__ == "__main__":
+    main()
