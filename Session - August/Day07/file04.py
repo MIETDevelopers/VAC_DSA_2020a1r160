c62fc94a0df9d788e40e024d4fead15fb1a6a23a
samragyi <samragyi1212@gmail.com>
2023-08-23 14:02:51 +0530

    files added

--- Session - August/Day07/file04.py
+++ Session - August/Day07/file04.py
@@ -2,4 +2,24 @@
 full binary tree
 
 
+infix to prefix
+reverse: f+(e*d) -c/b+a
+
+f            f
++      +     f
+(      +(    f
+e            fe
+*       +(*  fe
+d             fed
+)       +      fed*
+-       -      fed*+
+c               c
+/       -/     fed*+c
+b              fed*+cb
++       +       fed*+cb/-
+a               fed*+cb/-a
+fed*+cb/-a+
+
+reverse: +a-/bc+*def
 """
+
