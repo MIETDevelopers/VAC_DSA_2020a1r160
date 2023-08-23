28df1a8d0cb1b16de0a761dae1b4c8203cf1cb50
samragyi <samragyi1212@gmail.com>
2023-08-23 11:52:13 +0530

    files added

--- Session - August/Day07/file03.py
+++ Session - August/Day07/file03.py
@@ -23,14 +23,15 @@ op: kcehc olleh
 ip: hello
 op: hello
 """
-
+"""
 def swap_and_reverse(s):
     words = s.split(" ")
     n = len(words)-1
-    for i in range(len(words) - 1):
+    new = ""
+    for i in range(n):
         if len(words[i]) == len(words[n-i]):
-            #print(words[i])
-            #print(words[n-i])
+                #print(words[i])
+                #print(words[n-i])
             words[i], words[n-i] = words[n-i], words[i]
             words[i] = words[i][::-1]
             words[n - i] = words[n-i][::-1]
@@ -38,3 +39,20 @@ def swap_and_reverse(s):
 
 user_input = input("Enter a string: ")
 print(swap_and_reverse(user_input))
+
+    """
+def swap_reverse(s):
+    n = len(s)
+    a = []
+    for i in range(0,n):
+        if len(s[i]) == len(s[n-i-1]):
+            if s[i] == s[n-i-1]:
+                a.append(s[i])
+            else:
+                a.append(s[n-i-1][::-1])
+        else:
+            a.append(s[i])
+    print(a)
+    
+user_input = input("Enter a string: ").split(" ")
+swap_reverse(user_input)
