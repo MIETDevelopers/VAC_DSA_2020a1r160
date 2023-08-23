a67102f8ca870bf56d01cce249ceb32a4933b2c1
samragyi <samragyi1212@gmail.com>
2023-08-23 10:43:43 +0530

    files added

---
+++ Session - August/Day07/file03.py
@@ -0,0 +1,26 @@
+"""
+ reverse words only if ith and n-ith have same length on comparing
+ 
+ 
+ip: hello hai check
+op: kcehc hai olleh
+
+ip: hello hai bye check
+op: kcehc eyb iah olleh
+
+ip: hello hai check bye
+op: hello hai check bye
+
+ip: hello hai bye chennai
+op: hello yeb iah chennai
+
+ip: hello chennai am in kashmir hotts
+op: sttoh rimhsak ni ma iannehc olleh
+
+ip: hello check
+op: kcehc olleh
+
+ip: hello
+op: hello
+"""
+
