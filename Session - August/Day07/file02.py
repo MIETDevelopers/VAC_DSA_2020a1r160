1fd0e0ee97ebf396acda5933d3fe731d15c998b9
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-08-23 10:14:08 +0530

    files added

--- Session - August/Day07/file02.py
+++ Session - August/Day07/file02.py
@@ -14,3 +14,15 @@ ip: apple, angle, aloevera
 
 output: no its false acronym
     """
+
+def is_acronym(word_list, acronym):
+    formed_acronym = ''.join(word[0] for word in word_list)
+    return formed_acronym == acronym
+
+input_words = input("Enter a list of words separated by commas: ").split(',')
+acronym = input("Enter the acronym to check: ")
+
+if is_acronym(input_words, acronym):
+    print("Yes, it is an acronym.")
+else:
+    print("No, it is not an acronym.")
