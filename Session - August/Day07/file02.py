87cf4cd98f578eef2a981370ad9e40859355b288
samragyi <samragyi1212@gmail.com>
2023-08-23 10:10:52 +0530

    files added

---
+++ Session - August/Day07/file02.py
@@ -0,0 +1,16 @@
+"""
+ip: apple, banana, cat, zebra, cinderella
+    : abczc
+
+output: yes its true acronym
+
+ip: apple, angle, banana, zebra
+    : abz
+
+output: no its false acronym
+
+ip: apple, angle, aloevera
+    : aa
+
+output: no its false acronym
+    """
