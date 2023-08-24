ba7318c40d1147b78013a928bff9d8a5ee9de0c9
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-08-24 14:55:48 +0530

    files added

--- Session - August/Day08/plotProfit.py
+++ Session - August/Day08/plotProfit.py
@@ -4,15 +4,17 @@ def calculate_max_profit(s, n):
     for pair in s:
         plot1, plot2, profit = pair
         
+        # Conditions
         if (plot1 >= n or plot2 >= n) or plot1 in plots or plot2 in plots:
             return "no profit"
         
         plots.add(plot1)
         plots.add(plot2)
-        max_profit = max(max_profit, profit)
-    print(plots)
+        
+        # Compare and get maximum of a3, b3, c3
+        max_value = max(plot1, plot2, profit)
+        max_profit = max(max_profit, max_value)
     return max_profit
-
 
 n = int(input("Enter the value of n: "))
 m = int(input("Enter the number of nested lists: "))
@@ -22,5 +24,4 @@ for _ in range(m):
     s.append([plot1, plot2, profit])
     
 result = calculate_max_profit(s, n)
-print("profit:", result)
-
+print("Profit:", result)
