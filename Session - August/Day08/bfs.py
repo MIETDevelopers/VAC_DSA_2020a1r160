9e55e888b158d0a19ec8adeed1d5edf8277d78d3
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-08-24 13:27:32 +0530

    files added

---
+++ Session - August/Day08/bfs.py
@@ -0,0 +1,19 @@
+#program to demostrate breadth first search
+def bfs(graph, start):
+    visited = []
+    queue = [start]
+    while queue:
+        node = queue.pop(0)
+        if node not in visited:
+            visited.append(node)
+            neighbours = graph[node]
+            for neighbour in neighbours:
+                queue.append(neighbour)
+    return visited
+graph = {'A': ['B', 'C'],
+         'B': ['D', 'E'],
+         'C': ['F'],
+         'D': [],
+         'E': ['F'],
+         'F': []}
+print(bfs(graph, 'A'))
