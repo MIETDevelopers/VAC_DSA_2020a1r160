075ebdce564a5105850c6be0c3db5117f40227fb
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-08-25 15:50:40 +0530

    files added

--- Session - August/Day09/file02.py
+++ Session - August/Day09/file02.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 def printNum(n):
     n = n*100
     for i in range(1, n+1):
         print(i/100)
 inp = int(input("Enter a number: "))
 printNum(inp)
-=======
-i=2
-while(i<=3):
-    print("%.3f"%i)
-    i+=0.001
->>>>>>> 261aa299aaf8825f81dbbda06e772ba596512d3b
