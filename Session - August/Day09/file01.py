8e0a8a78d52858fc2edb10fa88db7efce1224786
Ishav Verma <ishu03.ishavverma@gmail.com>
2023-08-25 13:17:34 +0530

    files added

---
+++ Session - August/Day09/file01.py
@@ -0,0 +1,10 @@
+#Program to check that input string in panagram or not
+def panagram(str):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabet:
+        if char not in str.lower():
+            return False
+    return True
+string = input("Enter the string: ")
+if(panagram(string) == True):
+    print("Yes, it is panagram")
