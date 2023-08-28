8cacfa0c64de30500dfd166a53e19f779cc90a56
Lavanya Sharma <lavanya2210sharma@gmail.com>
2023-08-28 10:13:49 +0530

    new

---
+++ Session - August/Day09/file03.py
@@ -0,0 +1,13 @@
+#print number from 0.0 to 1.0 in 0.1 increments
+
+# with this one:
+
+for i in range(10):
+
+    print((i+1)/10)
+
+#print number from 2.00 to 3.00 in 0.1 increments
+
+for i in range(10):
+    print((i+2)/10)
+    
